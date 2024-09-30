8d437a47aaab71e7887db5bf40d29c367553e060
Daniel <daniel20021904@gmail.com>
2024-09-29 21:58:12 -0500

    API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Agrupar las rutas bajo el prefijo 'v1'
+Route::prefix('v1')->group(function () {
+    Route::apiResource('productos', ProductoController::class);
 });
